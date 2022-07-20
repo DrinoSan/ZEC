880aa2eb1f6db67ba2672a0f31d8a2874d6669f8
DrinoSan <sbed30@icloud.com>
2022-07-20 22:16:48 +0200

    Adding GROW_ARRAY macro for allocating new array

--- memory.h
+++ memory.h
@@ -3,6 +3,10 @@
 
 #include "common.h"
 
-#define GROW_CAPACITY(capacity) / ((capacity) < 8 ? 8 : (capacity)*2)
+#define GROW_CAPACITY(capacity) \ 
+    ((capacity) < 8 ? 8 : (capacity)*2)
 
+#define GROW_ARRAY(type, pointer, oldCount, newCount) \
+    (type*)realloc(pointer, sizeof(type) * (oldCount), \
+    sizeof(type) * (newCount))
 #endif
